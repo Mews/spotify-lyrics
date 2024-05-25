d9b23c7888e21598549481b31b50663835f7af5c
Mews <ar754456@gmail.com>
2024-05-25 12:13:16 +0100

    Fixed issue where the time indicator would get cropped when the window got too small

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import settings
 ui = MainUi(title="Spotify Lyrics Client", themename=settings.theme)
 
 ui.geometry("1100x700")
-ui.minsize(825, 575)
+ui.minsize(935, 575)
 ui.iconbitmap("assets/icon.ico")
 
 ui.mainloop()
